9d6d08d88c580d67911ae2a84f35bcb185b0008a
Joshua Jackson <j.shuajackson@gmail.com>
2020-01-24 19:43:43 -0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '115519799819646',
+        clientSecret: '5ab7f1af9bf2e582f635d0931d8e0431'
+    }
 }
