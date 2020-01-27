ef595a502a981e130b6f246f7423fc9bb818f147
Joshua Jackson <j.shuajackson@gmail.com>
2020-01-26 16:22:30 -0800

    Assignment 4

--- routes/users.js
+++ routes/users.js
@@ -9,15 +9,16 @@ const router = express.Router();
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-  User.find({})
-    .then((users) => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json(users);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-});
+router.get('/', cors.corsWithOptions, authenticate.verifyUser,
+  authenticate.verifyAdmin, (req, res, next) => {
+    User.find({})
+      .then((users) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(users);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+  });
 
 router.post('/signup', cors.corsWithOptions, (req, res, next) => {
   User.register(new User({ username: req.body.username }),
